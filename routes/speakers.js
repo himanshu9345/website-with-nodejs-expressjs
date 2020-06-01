c4e897378bb6664fcc777ca2a12f1bb0dfd64dbb
himanshu9345 <hbp53@njit.edu>
2020-06-01 17:50:19 -0400

    worked on parameterized routes

--- routes/speakers.js
+++ routes/speakers.js
@@ -6,12 +6,24 @@ const router = express.Router();
 module.exports = (params) => {
   // destructuring of params
   const { speakersService } = params;
+  // router.get('/', async (request, response) => {
+  //   const speakers = await speakersService.getList();
+  //   return response.json(speakers);
+  // });
+
   router.get('/', async (request, response) => {
+    // if (!request.session.visitcount) {
+    //   request.session.visitcount = 0;
+    // }
+    // request.session.visitcount += 1;
+    // console.log(`Number of visits: ${request.session.visitcount}`);
     const speakers = await speakersService.getList();
-    return response.json(speakers);
+    response.render('layout', { pageTitle: 'Speakers', template: 'speakers', speakers });
   });
-  router.get('/:jobid', (request, response) => {
-    response.send(`Detail page of ${request.params.jobid}`);
+
+  router.get('/:shortname', async (request, response) => {
+    const speaker = await speakersService.getSpeaker(request.params.shortname);
+    response.render('layout', { pageTitle: 'Speakers', template: 'speakers-detail', speaker });
   });
   return router;
 };
