e79c8ba4b773e02871d3cc2d14c23f96e224bd55
himanshu9345 <hbp53@njit.edu>
2020-06-07 21:11:06 -0400

    created a form template

--- routes/feedback.js
+++ routes/feedback.js
@@ -5,6 +5,26 @@ const router = express.Router();
 
 module.exports = (params) => {
   const { feedbackService } = params;
+
+  router.get('/', async (request, response, next) => {
+    try {
+      const feedback = await feedbackService.getList();
+      // console.log(allartwork);
+      return response.render('layout', {
+        pageTitle: 'Feedback',
+        template: 'feedback',
+        feedback,
+      });
+    } catch (error) {
+      return next(error);
+    }
+    // if (!request.session.visitcount) {
+    //   request.session.visitcount = 0;
+    // }
+    // request.session.visitcount += 1;
+    // console.log(`Number of visits: ${request.session.visitcount}`);
+  });
+
   router.get('/', async (request, response, next) => {
     try {
       // await will wait till promise returns, so fn is async
